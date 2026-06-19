6ab59257a6d507e6fb2b3ed8a810c6ba6134ebe0
Gisele Ervolino <gisele.ervolino@gmail.com>
2026-06-19 17:34:59 -0300

    AAA - Arrange, Act, Assert

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,27 +1,20 @@
 import { test, expect } from '@playwright/test'
 
+// AAA - Arrange, Act, Assert
 test('deve consultar um pedido aprovado', async ({ page }) => {
+  //Arrange 
   await page.goto('http://localhost:5173/')
-  
-  //Checkpoint
   await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
-
   await page.getByRole('link', { name: 'Consultar Pedido' }).click()
-
-  //Checkpoint
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
+  //Act
   await page.getByTestId('search-order-id').fill('VLO-712MP6')
-
   await page.getByTestId('search-order-button').click()
 
-  //Checkpoint  
+  //Assert
   await expect(page.getByTestId('order-result-id')).toBeVisible()
-
   await expect(page.getByTestId('order-result-id')).toContainText('VLO-712MP6')
-
   await expect(page.getByTestId('order-result-status')).toBeVisible()
-
   await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
-  
 })
