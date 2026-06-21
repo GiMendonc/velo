fca01098210679871beb15ccb210a98f024a62f2
Gisele Ervolino <gisele.ervolino@gmail.com>
2026-06-21 19:44:27 -0300

    Code Challenge: Você consegue testar sem Test-ID?

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -9,12 +9,17 @@ test('deve consultar um pedido aprovado', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
 
   //Act
-  await page.getByTestId('search-order-id').fill('VLO-712MP6')
-  await page.getByTestId('search-order-button').click()
+  await page.getByLabel('Número do Pedido').fill('VLO-712MP6')
+  await page.getByRole('button', { name: 'Buscar Pedido' }).click()
 
   //Assert
-  await expect(page.getByTestId('order-result-id')).toBeVisible()
-  await expect(page.getByTestId('order-result-id')).toContainText('VLO-712MP6')
-  await expect(page.getByTestId('order-result-status')).toBeVisible()
-  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  // await expect(page.getByTestId('order-result-id')).toBeVisible({ timeout: 10000 })
+  // await expect(page.getByTestId('order-result-id')).toContainText('VLO-712MP6')
+  // await expect(page.getByTestId('order-result-status')).toBeVisible()
+  // await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+
+  await expect(page.getByText('VLO-712MP6')).toBeVisible({timeout: 10000})
+  await expect(page.getByTestId('order-result-VLO-712MP6')).toContainText('VLO-712MP6')
+  await expect(page.getByText('APROVADO')).toBeVisible()
+  await expect(page.getByTestId('order-result-VLO-712MP6')).toContainText('APROVADO')
 })
